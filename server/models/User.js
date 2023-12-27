aa59c96b717e860020fa480110f8fba3cced5baf
Kinshuk Dwivedi <007kinshuk@gmail.com>
2023-12-27 16:49:36 +0530

    Initial commit

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+import Mongoose from "mongoose";
+
+const userSchema = new Mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = Mongoose.model("User", userSchema);
+export default User;
